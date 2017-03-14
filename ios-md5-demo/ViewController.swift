81eda0380e4399a77c69f380b05dabdbf035d5c9
stv-ekushida <e.kushida@st-ventures.jp>
2017-03-14 21:44:39 +0900

    ios-hash-md5

---
+++ ios-md5-demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios-md5-demo
+//
+//  Created by Kushida　Eiji on 2017/03/14.
+//  Copyright © 2017年 Kushida　Eiji. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("12345".md5)
+    }
+}
+
